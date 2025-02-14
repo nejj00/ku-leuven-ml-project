b95944bfc1d81dce5c430a4a6093eecb753ca28c
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2025-02-14 17:48:09 +0100

    fix submission.py
    - typing.Callable cannot be extended
    - some formatting

--- submission.py
+++ submission.py
@@ -1,5 +1,3 @@
-from typing import Callable
-
 import gymnasium
 from pettingzoo.utils import BaseWrapper
 from pettingzoo.utils.env import AgentID, ObsType
@@ -7,7 +5,7 @@ from pettingzoo.utils.env import AgentID, ObsType
 
 class CustomWrapper(BaseWrapper):
     """
-     Wrapper to use to add state pre-processing (feature engineering)
+    Wrapper to use to add state pre-processing (feature engineering).
     """
 
     def observation_space(self, agent: AgentID) -> gymnasium.spaces.Space:
@@ -17,9 +15,10 @@ class CustomWrapper(BaseWrapper):
         pass
 
 
-
-class CustomPredictFunction(Callable):
-    """ Function to use to load the trained model and predict the action"""
+class CustomPredictFunction:
+    """
+    Function to use to load the trained model and predict the action.
+    """
 
     def __init__(self, env: gymnasium.Env):
         pass
